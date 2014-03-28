8d8a98fc83298ac25d446dee878c59b1d2f3f212
Chris Morgan <me@chrismorgan.info>
2014-03-28 11:07:15 +1100

    Add model path to polymorphic serializer output.
    At present this is the Python module path. Using that will make it
    genuinely easy to implement from_native when we need to. We could switch
    it to using names of another form without significant difficulty—it'd
    just be introducing a lookup of some form.

--- cqrs/models.py
+++ cqrs/models.py
@@ -10,5 +10,17 @@ class CQRSModel(CQRSModelMixin):
 
 class CQRSPolymorphicModel(CQRSModelMixin, PolymorphicModel):
 
+    @property
+    def _type_path(self):
+        '''
+        Get the model's path. Used by the serializer.
+
+        (We could use the ContentType, but that'd be rather inefficient and
+        unnecessary; django-polymorphic has already done that for us by giving
+        us an instance of the right type.)
+        '''
+        type_ = type(self)
+        return '{}.{}'.format(type_.__module__, type_.__name__)
+
     class Meta:
         abstract = True
