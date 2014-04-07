10cebde996df44af13a5de3b4659798e2d060420
Chris Morgan <me@chrismorgan.info>
2014-04-07 14:43:42 +1000

    Move serialized id → _id for MongoDB.
    This stops us from making a mess all over our database, with ``_id``
    being there some of the time and not at other times, leading to gross
    duplication of entries.

--- cqrs/mongo.py
+++ cqrs/mongo.py
@@ -4,11 +4,22 @@ from .backend import PolymorphicBackendBase
 from denormalize.backend.mongodb import MongoBackend
 
 
-class PolymorphicMongoBackend(MongoBackend, PolymorphicBackendBase):
+class MongoIDBackend(MongoBackend):
+
+    def added(self, collection, doc_id, doc):
+        doc['_id'] = doc.pop('id')
+        super(MongoIDBackend, self).added(collection, doc_id, doc)
+
+    def changed(self, collection, doc_id, doc):
+        doc['_id'] = doc.pop('id')
+        super(MongoIDBackend, self).changed(collection, doc_id, doc)
+
+
+class PolymorphicMongoIDBackend(MongoIDBackend, PolymorphicBackendBase):
     pass
 
 
-mongodb = PolymorphicMongoBackend(
+mongodb = PolymorphicMongoIDBackend(
     name='mongo',
     db_name=settings.CQRS_MONGO_DB_NAME,
     connection_uri=settings.CQRS_MONGO_CONNECTION_URI
